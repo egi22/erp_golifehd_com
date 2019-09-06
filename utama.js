a38e6a19586c6e83a8b45566fdab3c3b0c44285a
egi22 <46200069+egi22@users.noreply.github.com>
2019-09-06 19:11:53 +0700

    Update utama.js

--- utama.js
+++ utama.js
@@ -27,7 +27,7 @@ if(x=="6"){xxx="/2019/08/cloud-based-ERP-systems-for-manufacturing.html"}
 if(x=="7"){xxx="/2019/08/manufacturing-software-for-quickbooks.html"}
 if(x=="8"){xxx="/2019/08/production-management-system-software.html"}
 if(x=="9"){xxx="/2019/08/cost-accounting-software-for-manufacturing.html"}
-if(x=="10"){xxx="/2019/08/manufacturing-software-free-download.html"}a_to_ve[i].href="http://erp.golifehd.com"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
+if(x=="10"){xxx="/2019/08/manufacturing-software-free-download.html"}a_to_ve[i].href="https://erp.golifehd.com"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
